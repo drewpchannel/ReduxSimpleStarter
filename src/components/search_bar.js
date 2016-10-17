157468431dbc3d7b89177ed19a255f1fdd98f9be
Drew Poulin <drewpcchannel@gmail.com>
2016-10-17 14:05:45 -0400

    cont tutorial

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,13 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 
 class SearchBar extends Component{
-  render() {
-    return <input onChange={this.onInputChange} />;
-  }
+  constructor(props) {
+    super(props);
 
-  onInputChange() {
-    
+    this.state = { term: ''};
+  }
+  render() {
+    return ( 
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    );
   }
 }
 
