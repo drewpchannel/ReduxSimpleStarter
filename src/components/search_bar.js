3ef4b546d9c3d59046ce20a51e5de46a1d5c3486
Drew Poulin <drewpcchannel@gmail.com>
2016-10-17 12:54:41 -0400

    starter react

---
+++ src/components/search_bar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class SearchBar extends Component{
+  render() {
+    return <input onChange={this.onInputChange} />;
+  }
+
+  onInputChange() {
+    
+  }
+}
+
+export default SearchBar;
